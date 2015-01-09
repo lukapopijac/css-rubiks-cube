9e34b5cd1ba07ba267798286a961e8472f7603b1
Luka Popijac <luka.popijac@gmail.com>
2015-01-08 21:45:07 -0800

    add message to use chrome

---
+++ useragent-message.js
@@ -0,0 +1,14 @@
+﻿if(navigator.userAgent.toLowerCase().indexOf('chrome') == -1) {
+	window.addEventListener('load', function() {
+		var el = document.createElement('span');
+		el.innerHTML = 'Your browser may not be able to support this demo. Please use Google Chrome.';
+		el.style.background = 'red';
+		el.style.color      = 'white';
+		el.style.fontSize   = '20px';
+		el.style.padding    = '2px';
+		el.style.maxWidth   = '65%';
+		el.style.display    = 'inline-block';
+		
+		document.body.insertBefore(el, document.body.firstChild);
+	}, false);
+}
