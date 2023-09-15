817419e3d011235e9561e7bd059c832fac9881d0
Luka Popijac <luka.popijac@gmail.com>
2023-09-15 16:35:43 -0700

    remove old gh-pages action

--- github-link.js
+++ github-link.js
@@ -2,26 +2,30 @@ class GitHubLink extends HTMLElement {
 	constructor() {
 		super();
 		this.attachShadow({mode: 'open'});
+	}
+	connectedCallback() {
 		this.shadowRoot.innerHTML = `
-            <style>
-                a {
-                    position: absolute;
-                    top: 14px;
-                    right: 14px;
-                    font-family: sans-serif;
-                    font-weight: normal;
-                    font-size: 16px;
-                    color: blue;
-                    cursor: pointer;
-                    overflow: hidden;
-                    pointer-events: auto;
-                    text-decoration: none;
-                    border: 1px solid blue;
-                    padding: 6px 10px;
-                    border-radius: 4px;
-                }
-            </style>
-			<a href="https://github.com/lukapopijac/css-rubiks-cube">GitHub</a>
+			<style>
+				a {
+					position: absolute;
+					top: 14px;
+					right: 14px;
+					font-family: sans-serif;
+					font-weight: normal;
+					font-size: 16px;
+					line-height: normal;
+					color: blue;
+					cursor: pointer;
+					overflow: hidden;
+					pointer-events: auto;
+					text-decoration: none;
+					border: 1px solid blue;
+					padding: 6px 10px;
+					border-radius: 4px;
+					background: rgba(255,255,255,0.3);
+				}
+			</style>
+			<a href="${this.getAttribute('href')}">GitHub</a>
 		`;
 	}
 }
